ef68443a09e9af7cb4e7703db1c2888f0af5b135
Nihility.io <143349546+Nihility-io@users.noreply.github.com>
2024-07-11 15:16:46 +0200

    Fix Deno Fresh build being stuck

--- fresh.config.ts
+++ fresh.config.ts
@@ -3,8 +3,11 @@ import { defineConfig } from "$fresh/server.ts"
 import { LangPlugin } from "@nihility-io/fresh-lang"
 import { Secrets } from "secret/server"
 
-// Trigger the garbage collector on startup
-Secrets.shared.garbageCollection()
+const isBuildMode = Deno.args.includes("build")
+if (!isBuildMode) {
+	// Trigger the garbage collector on startup but not when building
+	Secrets.shared.garbageCollection()
+}
 
 export default defineConfig({
 	plugins: [
