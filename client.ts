87c43bea071d5f0f11af2005acf06e717710dd77
Marvin Peter <mail@marvinpeter.com>
2025-09-12 09:18:31 +0200

    Fix file uploads on non-Safari browsers by adding a polyfill for Blob.bytes

--- client.ts
+++ client.ts
@@ -1,1 +1,9 @@
 import "./static/styles.css"
+
+// Polyfill for Blob.bytes() in browsers that don't support it yet
+// https://developer.mozilla.org/en-US/docs/Web/API/Blob/bytes
+if (Blob.prototype.bytes === undefined) {
+	Blob.prototype.bytes = function (): Promise<Uint8Array<ArrayBuffer>> {
+		return this.arrayBuffer().then((buf) => new Uint8Array(buf))
+	}
+}
